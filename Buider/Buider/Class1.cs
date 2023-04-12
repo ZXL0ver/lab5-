9c08f507c252ab3f0c34f797775bdb88296034a3
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:15:54 +0300

    Add files via upload

---
+++ Buider/Buider/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buider
+{
+    public abstract class lantern
+    {
+        public abstract lantern Copy();
+    }
+
+    public class lantarn : lantern
+    {
+        public string pillar, wiring, lamp;
+        public string Data() => pillar + "\n" + wiring + "\n" + lamp + "\n";
+        public void GetPillar(string dat) => pillar = dat;
+        public void GetWiring(string dat) => wiring = dat;
+        public void GetLamp(string dat) => lamp = dat;
+
+
+        public override lantern Copy()
+        {
+            lantarn clone = (lantarn)this.MemberwiseClone();
+            clone.pillar = String.Copy(pillar);
+            clone.wiring = String.Copy(wiring);
+            clone.lamp = String.Copy(lamp);
+            return clone;
+        }
+    }
+
+    public interface Builder
+    {
+        void BuildPillar();
+
+        void BuildWiring();
+
+        void BuildLamp();
+        lantarn GetLamp();
+    }
+    public class lanternBuilder : Builder
+    {
+        private lantarn lat;
+
+        public lanternBuilder()
+        {
+            lat = new lantarn();
+        }
+
+        public void BuildPillar()
+        {
+            lat.GetPillar("pillar");
+        }
+
+        public void BuildWiring()
+        {
+            lat.GetWiring("Wiring");
+        }
+
+        public void BuildLamp()
+        {
+            lat.GetLamp("lamp");
+        }
+        public lantarn GetLamp() => lat;
+    }
+    public class GoodlanternBuilder : Builder
+    {
+        private lantarn lat;
+
+        public GoodlanternBuilder()
+        {
+            lat = new lantarn();
+        }
+        public void BuildPillar()
+        {
+            lat.GetPillar("good pillar");
+        }
+        public void BuildWiring()
+        {
+            lat.GetWiring("good Wiring");
+        }
+
+        public void BuildLamp()
+        {
+            lat.GetLamp("awaseme lamp");
+        }
+        public lantarn GetLamp() => lat;
+
+    }
+    public class Director
+    {
+        private Builder builder;
+
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+
+        }
+        public void Choselantern(Builder builder)
+        {
+            this.builder = builder;
+        }
+
+        public lantarn Buildpiz()
+        {
+            builder.BuildPillar();
+            builder.BuildWiring();
+            builder.BuildLamp();
+            return builder.GetLamp();
+        }
+
+    }
+}
---
+++ Buider/Buider/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Buider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Builder build = new lanternBuilder();
+            Director director = new Director(build);
+            lantarn lat = director.Buildpiz();
+            richTextBox1.Text += lat.Data();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Builder build = new GoodlanternBuilder();
+            Director director = new Director(build);
+            lantarn lat = director.Buildpiz();
+            richTextBox1.Text += lat.Data();
+            for (int i = 0; i < 3; i++)
+            {
+                lantern clone = lat.Copy();
+                richTextBox1.Text += "Копія" + (i + 1) + " " + ((lantarn)clone).pillar + ((lantarn)clone).pillar + ((lantarn)clone).wiring + "\n";
+            }
+        }
+    }
+}
